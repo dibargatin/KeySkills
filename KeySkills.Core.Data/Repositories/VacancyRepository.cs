5802f4bc39cd950f27be985fdbeafb0a6debacc8
Dmitry Bargatin <dibargatin@gmail.com>
2020-06-06 14:32:15 +0400

    Implement an eager loading of keywords for VacancyRepository

--- KeySkills.Core.Data/Repositories/VacancyRepository.cs
+++ KeySkills.Core.Data/Repositories/VacancyRepository.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using KeySkills.Core.Models;
 using KeySkills.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace KeySkills.Core.Data.Repositories
 {
     public class VacancyRepository : BaseRepository<Vacancy, BaseDbContext>, IVacancyRepository
     {
         public VacancyRepository(BaseDbContext context) : base(context) {}
+
+        private IQueryable<Vacancy> GetVacancies() =>
+            _context.Set<Vacancy>()
+                .Include(v => v.Keywords)
+                .ThenInclude(vk => vk.Keyword);
+
+        public override async Task<IEnumerable<Vacancy>> GetAllAsync() =>
+            await GetVacancies().ToListAsync();
+
+        public override async Task<IEnumerable<Vacancy>> GetAsync(Func<Vacancy, bool> predicate) =>
+            await GetVacancies().Where(e => predicate(e)).ToListAsync();
     }
 }
