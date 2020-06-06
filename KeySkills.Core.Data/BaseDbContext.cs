e0f219355d6b9eda69f782927f2ccf5878d7e8b2
Dmitry Bargatin <dibargatin@gmail.com>
2020-06-06 13:14:42 +0400

    Setup model creation for BaseDbContext

--- KeySkills.Core.Data/BaseDbContext.cs
+++ KeySkills.Core.Data/BaseDbContext.cs
@@ -1,5 +1,6 @@
 using KeySkills.Core.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KeySkills.Core.Data
 {
@@ -9,5 +10,58 @@ namespace KeySkills.Core.Data
 
         public DbSet<Vacancy> Vacancies { get; set; }
         public DbSet<Keyword> Keywords { get; set; }
+        public DbSet<VacancyKeyword> VacancyKeywords { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Keyword 
+            modelBuilder.Entity<Keyword>()
+                .Property(k => k.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Keyword>()
+                .Property(k => k.Pattern)
+                .IsRequired();
+
+            // Vacancy
+            modelBuilder.Entity<Vacancy>()
+                .HasIndex(v => v.Link)
+                .IsUnique();
+
+            modelBuilder.Entity<Vacancy>()
+                .Property(v => v.Link)
+                .IsRequired();
+
+            modelBuilder.Entity<Vacancy>()
+                .Property(v => v.Title)
+                .IsRequired();
+
+            modelBuilder.Entity<Vacancy>()
+                .Property(v => v.Description)
+                .IsRequired();
+
+            modelBuilder.Entity<Vacancy>()
+                .Property(v => v.PublishedAt)
+                .IsRequired();
+
+            modelBuilder.Entity<Vacancy>()
+                .Property(v => v.CountryCode)
+                .HasMaxLength(2)
+                .HasConversion(new EnumToStringConverter<Country>());
+
+            // VacancyKeyword
+            modelBuilder.Entity<VacancyKeyword>()
+                .HasKey(vk => new { vk.VacancyId, vk.KeywordId });
+
+            modelBuilder.Entity<VacancyKeyword>()
+                .HasOne(vk => vk.Vacancy)
+                .WithMany(v => v.Keywords)
+                .HasForeignKey(vk => vk.VacancyId);
+
+            modelBuilder.Entity<VacancyKeyword>()
+                .HasOne(vk => vk.Keyword)
+                .WithMany(k => k.Vacancies)
+                .HasForeignKey(vk => vk.KeywordId);
+        }
     }
 }
